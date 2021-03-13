4f862d3a9902a5991d20f29b6a056565094cbd9d
Ricky "Shake'n'Bake" Bobby <39415009+ricky-shake-n-bake-bobby@users.noreply.github.com>
2021-03-12 23:03:06 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open('README.rst', 'rb') as f:
     README = f.read().decode('utf-8')
 
 os.environ['INIT_VERSION'] = version
-
+print('Init Version:' + os.environ['INIT_VERSION'])
 setup(
     name='interfax',
     version=version,
